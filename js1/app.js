3cfa71ddbaab9e94c207a64247c0f6ccbe15cf18
Muhammad khuzaima <khuzaimakhokhar001@gmail.com>
2026-01-16 17:51:42 +0500

    Add files via upload

---
+++ js1/app.js
@@ -0,0 +1,65 @@
+// DO While/While Loop
+
+// let i = 0;
+
+// while (i <= 3) {
+//     alert (i)
+//     i++;
+// }
+    
+
+
+// let i = 6;
+
+// do {alert(i);
+
+//     i++
+
+// } while (i <= 4);
+
+
+// local storage / session storage
+ 
+
+
+
+
+
+
+
+
+// let num1 = '5';
+// let num2 = 5;
+
+// document.write(num1*num2);
+
+
+// let studentArr = [];
+ function submit(){
+
+
+let name1 = document.getElementById("Name").value;
+let phone = document.getElementById("Phone").value;
+let email = document.getElementById("Email").value;
+let roll_no = document.getElementById("Roll").value;
+
+
+let student = {
+    name1 : name1,
+    phone:phone,
+    email:email,
+    roll_no:roll_no,
+
+
+}
+console.log("student",student);
+document.write("student",student)
+
+ }
+
+
+// function submit() {
+//     studentArr.push(name1,phone,email,roll);
+    
+// }
+// console.log('studentArr',studentArr);
